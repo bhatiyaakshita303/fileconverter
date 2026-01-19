ba550e733c6064a284a863cecbf5a8854baeb48a
akshita <bhatiyaakshita303@gmail.com>
2026-01-19 14:49:03 +0530

    update code

--- src/modules/image/image.convert.js
+++ src/modules/image/image.convert.js
@@ -3,7 +3,7 @@ const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
 
-const uploadDir = path.join(__dirname, "../../../uploads");
+const uploadDir = path.join(__dirname, "../uploads");
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
 
 async function convertImage(inputPath, format) {
