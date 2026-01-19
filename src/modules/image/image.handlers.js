d0e70eae6056a505feba799706e16b50c0e2e36e
akshita <bhatiyaakshita303@gmail.com>
2026-01-19 11:42:50 +0530

    Expose uploads folder

--- src/modules/image/image.handlers.js
+++ src/modules/image/image.handlers.js
@@ -21,7 +21,7 @@ async function handleConvert(req, res) {
 
     res.json({
       message: "Image converted successfully",
-      downloadUrl: `${req.protocol}://${req.get("host")}/uploads/${path.basename(outputPath)}`
+      output: outputPath
     });
 
   } catch (err) {
