bce13892d4aeae83c36ac96658cf8136e04c1ac6
akshita <bhatiyaakshita303@gmail.com>
2026-01-22 12:28:29 +0530

    code updated

--- src/server.js
+++ src/server.js
@@ -2,16 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors")
 const port = process.env.PORT || 5000;
-
+ 
 const imageRoutes = require("./routes/image.routes");
 const videoRoutes = require("./routes/video.routes");
 const pdfRoutes = require("./routes/pdf.routes");
 const audioRoutes = require("./routes/audio.routes");
-
+ 
 app.use(express.json());
+app.use(cors())
 app.use("/api/image", imageRoutes);
 app.use("/api/video", videoRoutes);
 app.use("/api/pdf", pdfRoutes);
 app.use("/api/audio", audioRoutes);
-
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+ 
+app.listen(port, () => console.log(`Server running on http://localhost:${ port }`));
